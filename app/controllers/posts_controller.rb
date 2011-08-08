f91550f355c7fcdfefa1d0a71592900200e7ea10
Sidharta Surya Kusnanto and Kim Choy <developers+chromeragnarok+kjchoy@roomorama.com>
2011-08-08 11:13:56 +0800

    Post Controller Finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,16 +13,39 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update_attributes(params[:post])
+      redirect_to(@post, :notice => 'Post was successfully updated')
+    else
+      render :action => "edit"
+    end
+
   end
 
   def edit
+    @post = current_user.posts.find(params[:id])
+
   end
 
   def destroy
+    @post = current_user.posts.find(params[:id])
+
+    if @post.destroy
+      redirect_to posts_path, :notice => "Blog Entry Deleted"
+    else
+      redirect_to posts_path, :notice => "Delete Failed"
+    end
+
   end
 
   def index
     @posts = current_user.posts.all
+  end
+
+  def show
+    @post = current_user.posts.find(params[:id])
+    render :action => "show"
   end
 
 end
